2163f12d4c9ba209a5ca9cc56820682d203a3104
DamBinh1 <118426540+DamBinh1@users.noreply.github.com>
2023-08-17 15:27:21 +0700

    Add files via upload

---
+++ laptrinhApc3/Session11/Program.cs
@@ -0,0 +1,29 @@
+﻿using laptrinhApc3.session11;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laptrinhApc3.session11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            BankAccount account = new BankAccount();
+            account.BalanceChanged += (sender, e) =>
+            {
+                Console.WriteLine("So du tai khoan da thay doi.");
+            };
+
+            account.Deposit(1000);
+            account.Withdraw(500);
+        }
+    }
+}
+
+
+
+
---
+++ laptrinhApc3/Session11/session11/BankAccount.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laptrinhApc3.session11
+{
+    public class BankAccount
+    {
+        private double balance;
+
+        public double Balance
+        {
+            get { return balance; }
+            private set
+            {
+                if (value >= 0)
+                {
+                    balance = value;
+                    OnBalanceChanged();
+                }
+                else
+                {
+                    Console.WriteLine("So du tai khoan khong the nho hon 0.");
+                }
+            }
+        }
+
+        public event EventHandler BalanceChanged;
+
+        public virtual void OnBalanceChanged()
+        {
+            BalanceChanged.Invoke(this, EventArgs.Empty);
+        }
+
+        public void Deposit(double amount)
+        {
+            if (amount > 0)
+            {
+                Balance += amount;
+                Console.WriteLine($"Gui tien thanh cong. So du moi: {Balance}");
+            }
+            else
+            {
+                Console.WriteLine("So tien gui phai lon hon 0.");
+            }
+        }
+
+        public void Withdraw(double amount)
+        {
+            if (amount > 0 && amount <= Balance)
+            {
+                Balance -= amount;
+                Console.WriteLine($"Rut tien thanh cong.So du moi: {Balance}");
+            }
+            else
+            {
+                Console.WriteLine("So tien rut khong hop le.");
+            }
+        }
+    }
+}
